e201138fe9764b5bdb0d62b42c77b5d33a2ba714
ArtMGreen <artyom5145@gmail.com>
2024-10-06 19:08:56 +0300

    CaptchaDataset custom class

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,38 +1,38 @@
-import albumentations
 import torch
-import numpy as np
+from torch.utils.data import Dataset
 
-from PIL import Image
-from PIL import ImageFile
+from torchvision.io import read_image, ImageReadMode
+from torchvision.transforms.v2.functional import convert_image_dtype
 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+import os
 
-class Dataset:
 
-    def __init__(self, image_paths, targets, resize = None):
-        self.image_paths = image_paths
-        self.targets = targets
-        self.resize = resize
-        self.aug = albumentations.Compose(
-            [
-                albumentations.Normalize(always_apply = True)
-            ]
-        )
+class CaptchaDataset(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.transform = transform
+        self.image_paths = [os.path.join(image_dir, img) for img in os.listdir(image_dir)]
+        self.labels = [self._get_label_from_filename(img) for img in os.listdir(image_dir)]
+
+    def _get_label_from_filename(self, filename):
+        label = filename.split('.')[0]  # Assuming the label is in the filename
+        encoded_label = []
+        for char in label:
+            if char.isdigit():
+                encoded_label.append(ord(char) - ord('0'))  # Encode digits as 0-9
+            elif char.isalpha():
+                encoded_label.append(ord(char.upper()) - ord('A') + 10)  # Encode letters as 10-35
+        return encoded_label
 
     def __len__(self):
         return len(self.image_paths)
 
-    def __getitem__(self, item):
+    def __getitem__(self, idx):
+        image = read_image(self.image_paths[idx], mode=ImageReadMode.RGB)
+        image = convert_image_dtype(image, dtype=torch.float)
+        label = self.labels[idx]
 
-        image = Image.open(self.image_paths[item]).convert("RGB")
-        label = self.targets[item]
-        if self.resize:
-            image = image.resize((self.resize[1], self.resize[0]), resample = Image.BILINEAR)
-        image = np.array(image)
-        image = self.aug(image = image)["image"]
-        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        if self.transform:
+            image = self.transform(image)
 
-        return {
-            "images": torch.tensor(image, dtype = torch.float),
-            "target": torch.tensor(label, dtype = torch.long)
-        }
+        return image, torch.tensor(label)
