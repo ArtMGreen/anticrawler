ea92fb40f19b669a0581b0ef231fc6744fec95c6
ArtMGreen <artyom5145@gmail.com>
2024-11-21 23:47:23 +0300

    Median filter speedup

--- defences/median_filter.py
+++ defences/median_filter.py
@@ -1,25 +1,22 @@
-import torch
-from torch import nn
+import cv2
 import numpy as np
-
+from torch import nn
+import torch
 
 class MedianFilter(nn.Module):
     def __init__(self, kernel_size):
         super(MedianFilter, self).__init__()
+        if kernel_size % 2 == 0:
+            raise ValueError("Kernel size must be an odd number.")
         self.kernel_size = kernel_size
 
     def forward(self, image):
-        ch, height, width = image.size()
-        image_filtered = torch.zeros_like(image)
-        kernel_half_size = self.kernel_size // 2
+        img_np = image.permute(1, 2, 0).cpu().numpy()  # (C, H, W) -> (H, W, C)
 
-        for i in range(height):
-            for j in range(width):
-                filter_x_start = np.clip(j - kernel_half_size, 0, width-1)
-                filter_x_end = np.clip(j + kernel_half_size + 1, 0, width-1)
-                filter_y_start = np.clip(i - kernel_half_size, 0, height-1)
-                filter_y_end = np.clip(i + kernel_half_size + 1, 0, height-1)
-                roi = image[:, filter_y_start:filter_y_end, filter_x_start:filter_x_end]
-                image_filtered[:, i, j] = torch.Tensor(np.median(roi, axis=(1, 2)))
+        filtered_np = np.stack(
+            [cv2.medianBlur(img_np[:, :, c], self.kernel_size) for c in range(img_np.shape[2])],
+            axis=2
+        )
 
-        return image_filtered
+        filtered_tensor = torch.from_numpy(filtered_np).permute(2, 0, 1)  # (H, W, C) -> (C, H, W)
+        return filtered_tensor
